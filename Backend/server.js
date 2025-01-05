3cb8245a90dc7f2c30375d3f3668ad7ccb66915c
Ziad Elshrief <zyad2100@gmail.com>
2025-01-05 20:46:33 +0200

    Added serving static folder from server

--- Backend/server.js
+++ Backend/server.js
@@ -1,24 +1,38 @@
 const express = require("express");
-const cors = require('cors')
-const port =process.env.PORT |  5000;
-const {errorHandler} = require("./middleware/errorMiddleware")
-const connectDB = require('./config/db')
-const bp = require('body-parser')
+const path = require("path");
+const cors = require("cors");
+const port = process.env.PORT | 5000;
+const { errorHandler } = require("./middleware/errorMiddleware");
+const connectDB = require("./config/db");
+const bp = require("body-parser");
 
-connectDB()
+connectDB();
 const app = express();
 
 let corsOptions = {
-  origin : ['http://localhost:5173'],
-}
+  origin: [process.env.FRONTEND_DEV_URL],
+};
 
-app.use(cors(corsOptions))
+app.use(cors(corsOptions));
 
-app.use(bp.json())
-app.use(bp.urlencoded({ extended: false }))
-app.use(errorHandler)
+app.use(bp.json());
+app.use(bp.urlencoded({ extended: false }));
 app.use("/api/goals", require("./routes/goalRoutes"));
 app.use("/api/users", require("./routes/userRoutes"));
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  app.get("*", (req, res) =>
+    res.sendFile(
+      path.resolve(__dirname, "../", "frontend", "dist", "index.html")
+    )
+  );
+} else {
+  app.get("/", (req, res) =>
+    res.send("Please convert to production enviroment")
+  );
+}
+app.use(errorHandler);
 app.listen(port, () =>
   console.log(`Server running on http://localhost:${port}`)
 );
