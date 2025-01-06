51a6d3252023978aaf8c9108efcc8ab0051043d1
Ziad Elshrief <zyad2100@gmail.com>
2025-01-06 22:28:18 +0200

    added update profile  to the ui and completed the user part of the frontend

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -14,13 +14,18 @@ import App from "./App.jsx";
 import Dashboard from "./pages/Dashboard.jsx";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
+import Profile from "./pages/Profile.jsx";
+import PrivateRoute from "./components/PrivateRoute.jsx";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<App />}>
-      <Route index={true} path="/" element={<Dashboard />} />
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
+      <Route path="" element={<PrivateRoute />}>
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/" element={<Dashboard />} />
+      </Route>
     </Route>
   )
 );
---
+++ frontend/src/pages/Profile.jsx
@@ -0,0 +1,119 @@
+import { useEffect, useState } from "react";
+import { FaUser } from "react-icons/fa";
+import { useSelector, useDispatch } from "react-redux";
+import { toast } from "react-toastify";
+import { Form, Button } from "react-bootstrap";
+import FormContainer from "../components/FormContainer";
+import { useUpdateUserMutation } from "../slices/usersApiSlice";
+import { setCredentials } from "../slices/authSlice";
+import Loader from "../components/Loader";
+
+export default function Profile() {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
+  const { name, email, password, confirmPassword } = formData;
+
+  const { userInfo } = useSelector((state) => state.auth);
+  const [updateProfile, { isLoading }] = useUpdateUserMutation();
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    setFormData((prev) => ({
+      ...prev,
+      name: userInfo.name,
+      email: userInfo.email,
+    }));
+  }, [userInfo]);
+
+  const onChange = (e) => {
+    setFormData((prev) => ({
+      ...prev,
+      [e.target.name]: e.target.value,
+    }));
+  };
+  const submitHandler = async (e) => {
+    e.preventDefault();
+    if (password !== confirmPassword) {
+      toast.error("Passwords do not match");
+    } else {
+      try {
+        const res = await updateProfile({
+          _id: userInfo._id,
+          name,
+          email,
+          password,
+        }).unwrap();
+        dispatch(setCredentials(res));
+        
+        toast.success("Profile Updated");
+      } catch (err) {
+        toast.error(err?.data?.message || err.error);
+      }
+    }
+  };
+  return (
+    <>
+      {isLoading ? (
+        <Loader />
+      ) : (
+        <FormContainer>
+          <section className="mx-auto text-center heading">
+            <h1>
+              <FaUser /> Update Profile
+            </h1>
+            <p>Check and update profile</p>
+          </section>
+          <Form onSubmit={submitHandler}>
+            <Form.Group className="my-2" controlId="name">
+              <Form.Label>Username</Form.Label>
+              <Form.Control
+                type="text"
+                name="name"
+                value={name}
+                placeholder="Enter your name"
+                onChange={onChange}
+              />
+            </Form.Group>
+            <Form.Group className="my-2" controlId="email">
+              <Form.Label>Email Address</Form.Label>
+              <Form.Control
+                type="email"
+                name="email"
+                value={email}
+                placeholder="Enter your email"
+                onChange={onChange}
+              />
+            </Form.Group>
+            <Form.Group className="my-2" controlId="password">
+              <Form.Label>Password</Form.Label>
+              <Form.Control
+                type="password"
+                name="password"
+                value={password}
+                placeholder="Enter your password"
+                onChange={onChange}
+              />
+            </Form.Group>
+            <Form.Group className="my-2" controlId="confrimPassword">
+              <Form.Label>Confirm Password</Form.Label>
+              <Form.Control
+                type="password"
+                name="confirmPassword"
+                value={confirmPassword}
+                placeholder="Confirm your password"
+                onChange={onChange}
+              />
+            </Form.Group>
+            <Button type="submit" variant="primary" className="mt-3">
+              Update
+            </Button>
+          </Form>
+        </FormContainer>
+      )}
+    </>
+  );
+}
